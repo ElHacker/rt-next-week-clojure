0b9e4defd4b8734fe5fc7d6b72e6e5ad80e4e211
elhacker <braulio.vladimir.cn@gmail.com>
2020-07-13 23:24:10 -0700

    Properly implement Hemitian smoothing

--- src/rt_in_weekend/core.clj
+++ src/rt_in_weekend/core.clj
@@ -158,7 +158,7 @@
                           ig (int (* 255.999 (vec/y corrected-color)))
                           ib (int (* 255.999 (vec/z corrected-color)))]]
                 (pixel-line ir ig ib))
-              "./images/perlin-smooth-spheres")))
+              "./images/perlin-hermitian-smooth-spheres")))
 
 
 (defn create-ppm []
